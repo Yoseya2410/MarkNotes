d09a52a538a848c21a62183441ac4b90011a4134
Yoseya <104573963+Yoseya2410@users.noreply.github.com>
2025-04-27 00:03:11 +0800

    Add files via upload

--- core/press.js
+++ core/press.js
@@ -1,22 +1,30 @@
 function loadMarkdownFile() {
-    document.getElementById('content').style.display = "block";
+    document.getElementById("content").style.display = "block";
     const urlParams = new URLSearchParams(window.location.search);
-    const selectedFile = urlParams.get('file') || 'main.md';
+    const selectedFile = urlParams.get("file") || "main.md";
 
     fetch(selectedFile)
-        .then(response => {
+        .then((response) => {
             if (!response.ok) {
-                throw new Error('File not found');
+                throw new Error("File not found");
             }
             return response.text();
         })
-        .then(markdownText => {
+        .then((markdownText) => {
             const htmlContent = marked.parse(markdownText);
-            document.getElementById('content').innerHTML = htmlContent;
-            generateToc();
+            document.getElementById("content").innerHTML = htmlContent;
+            generateToc(); //目录
+            hljs.highlightAll(); //代码高亮
+            //LaTex公式解析
+            renderMathInElement(document.body, {
+                delimiters: [
+                    { left: "$$", right: "$$", display: true },
+                    { left: "\\(", right: "\\)", display: false },
+                ],
+            });
         })
-        .catch(error => {
-            console.error('Error reading the file:', error);
-            window.location.href = '404.html';
+        .catch((error) => {
+            console.error("Error reading the file:", error);
+            window.location.href = "404.html";
         });
 }
